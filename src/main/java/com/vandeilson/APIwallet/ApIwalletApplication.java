bbb9981bbb2695c7e9f8e2e822c0d765cb8ffb33
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-03-18 14:47:21 -0300

    Initial commit

---
+++ src/main/java/com/vandeilson/APIwallet/ApIwalletApplication.java
@@ -0,0 +1,13 @@
+package com.vandeilson.APIwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIwalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vandeilson/APIwallet/ApIwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vandeilson.APIwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIwalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

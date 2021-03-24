cc4a521f65bac9deeeca7bca676f244bd12a5ee5
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-03-24 09:38:34 -0300

    implementado a documentacao com openapi3

--- src/main/java/com/vandeilson/APIwallet/ApIwalletApplication.java
+++ src/main/java/com/vandeilson/APIwallet/ApIwalletApplication.java
@@ -1,9 +1,13 @@
 package com.vandeilson.APIwallet;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Wallet API", version = "1.0",
+        description = "API REST for money transfer between users"))
 public class ApIwalletApplication {
 
 	public static void main(String[] args) {
